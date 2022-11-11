0d08776b02bdf813fa18092f63722349714468d6
Lorenzo <lorenzozemp@icloud.com>
2022-11-11 22:18:02 +1300

    Implemented structure and tweet btns

--- src/QuoteCard.js
+++ src/QuoteCard.js
@@ -1,12 +1,72 @@
-import React from "react";
-import Quote from "./Quote";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 export default function QuoteCard() {
+  const [quote, setQuote] = useState([]);
+
+  //Called on component load once
+  useEffect(() => {
+    fetch("https://animechan.vercel.app/api/random")
+      .then((response) => response.json())
+      .then((quoteObject) => {
+        setQuote(() => {
+          return {
+            quote: quoteObject.quote,
+            author: quoteObject.character,
+            anime: quoteObject.anime,
+          };
+        });
+      });
+  }, []);
+
+  //Handle Random Click Event
+  function handleRandomClick(event) {
+    setQuote(() => {
+      return {
+        quote: "",
+        author: "",
+        anime: "",
+      };
+    });
+    fetch("https://animechan.vercel.app/api/random")
+      .then((response) => response.json())
+      .then((quoteObject) => {
+        setQuote(() => {
+          return {
+            quote: quoteObject.quote,
+            author: quoteObject.character,
+            anime: quoteObject.anime,
+          };
+        });
+      });
+  }
+
   return (
     <div>
-      <Quote /> {/* Contains Quote, Author and Anime*/}
-      <button>tweet</button>
-      <button>random</button>
+      <div className="quote-wrapper">
+        <div className="quote">{quote.quote}</div>
+        <div className="author">Character: {quote.author}</div>
+        <div className="anime">Anime: {quote.anime}</div>
+      </div>
+      <button>
+        <a
+          href={`https://www.twitter.com/intent/tweet?hashtags=AniQuotes&text=${encodeURI(
+            quote.quote
+          )}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          tweet
+        </a>
+      </button>
+      <button
+        onClick={handleRandomClick}
+        disabled={
+          quote.quote === "" || quote.quote === undefined ? true : false
+        }
+      >
+        random
+      </button>
     </div>
   );
 }
